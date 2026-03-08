8e77feaffab34fb9d54961132d13f35c98e30fa1
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-08 06:27:53 +0300

    bug

--- dists/bin/learn_github.bat
+++ dists/bin/learn_github.bat
@@ -6,7 +6,7 @@ rem SPDX-FileCopyrightText: 2026 Сергей Леонтьев (leo@sai.msu.ru)
 echo "~dp0=%~dp0"
 echo "%LEARN_GITHUB%\bin\"
 echo "LEARN_GITHUB=%LEARN_GITHUB%"
-if NOT "%LEARN_GITHUB%bin\"=="%~dp0" (
+if NOT "%LEARN_GITHUB%\bin\"=="%~dp0" (
     echo "%0: FAIL, LEARN_GITHUB=%LEARN_GITHUB%" 1>&2
     exit /b 1
 )
