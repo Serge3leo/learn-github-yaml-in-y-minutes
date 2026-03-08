b354898d7c5b226406fd96fdf8d70df495f5204b
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-08 07:00:19 +0300

    Initial action (#11)

---
+++ dists/setvars.bat
@@ -0,0 +1,14 @@
+@echo off
+rem vim:set sw=4 ts=8 et fileencoding=utf8:
+rem SPDX-License-Identifier: BSD-2-Clause
+rem SPDX-FileCopyrightText: 2026 Сергей Леонтьев (leo@sai.msu.ru)
+
+if NOT exist "%~dp0\bin\learn_github.bat" (
+    echo "%0: FAIL: %~dp0\bin\learn_github.bat: Not found" 1>&2
+    exit /b 1
+)
+
+rem TODO: WARNING: У %LEARN_GITHUB% остаётся завершающий `\`
+
+set LEARN_GITHUB=%~dp0
+PATH %~dp0\bin;%PATH%
