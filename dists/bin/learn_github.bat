9b2f174368e490ba5534391b0c920fb214b8ff69
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-08 06:25:04 +0300

    dbg cache false

--- dists/bin/learn_github.bat
+++ dists/bin/learn_github.bat
@@ -4,7 +4,7 @@ rem SPDX-License-Identifier: BSD-2-Clause
 rem SPDX-FileCopyrightText: 2026 Сергей Леонтьев (leo@sai.msu.ru)
 
 echo "~dp0=%~dp0"
-echo "%LEARN_GITHUB%bin\"
+echo "%LEARN_GITHUB%\bin\"
 echo "LEARN_GITHUB=%LEARN_GITHUB%"
 if NOT "%LEARN_GITHUB%bin\"=="%~dp0" (
     echo "%0: FAIL, LEARN_GITHUB=%LEARN_GITHUB%" 1>&2
