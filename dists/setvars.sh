1e6281ce46b8e201930b48678faebcf3269aa039
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-10 04:53:50 +0300

    Python process setvars (#16)

--- dists/setvars.sh
+++ dists/setvars.sh
@@ -1,4 +1,4 @@
-#!
+#!/usr/bin/env bash
 # vim:set sw=4 ts=8 fileencoding=utf8:
 # SPDX-License-Identifier: BSD-2-Clause
 # SPDX-FileCopyrightText: 2026 Сергей Леонтьев (leo@sai.msu.ru)
@@ -11,4 +11,9 @@ d=$(dirname "$r")
 }
 LEARN_GITHUB="$d" ; export LEARN_GITHUB
 PATH="$d/bin:$PATH"
+# PATH="/opt/none:$PATH"
+# PATH="$PATH:$d/bin"
+LEARN_GITHUB_ML="lg line1
+lg line2
+lg line3" ; export LEARN_GITHUB_ML
 return 0
